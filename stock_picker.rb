95ec4cbf79472777712868936a0e32e67b35bc0a
Carlos Rojas <sadhu89@users.noreply.github.com>
2015-05-13 23:59:40 -0500

    Update stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,13 +3,13 @@ def stock_picker(prices)
 	imax=0
 	jmax=0
 	for i in 0..prices.length-1
-		for j in i+1..prices.length-1
-		  if prices[j]-prices[i]>diff
-        diff=prices[j]-prices[i]		
-				  imax=i
-					jmax=j
-			end
-		end
+	  for j in i+1..prices.length-1
+            if prices[j]-prices[i]>diff
+              diff=prices[j]-prices[i]		
+		 imax=i
+	         jmax=j
+	    end
+           end
 	end
 	return [imax,jmax]
 end
